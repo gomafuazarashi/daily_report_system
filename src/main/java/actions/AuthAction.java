0d097c8a30b6514c004fc10371915b3401d8a7de
赤﨑 将人 <masato.akasaki@nnlife.co.jp>
2023-06-02 10:32:24 +0900

    ログアウトアクションの作成

--- src/main/java/actions/AuthAction.java
+++ src/main/java/actions/AuthAction.java
@@ -33,11 +33,6 @@ public class AuthAction extends ActionBase {
         service.close();
     }
 
-    /**
-     * ログイン画面を表示する
-     * @throws ServletException
-     * @throws IOException
-     */
     public void showLogin() throws ServletException, IOException {
 
         //CSRF対策用トークンを設定
@@ -96,6 +91,24 @@ public class AuthAction extends ActionBase {
             //ログイン画面を表示
             forward(ForwardConst.FW_LOGIN);
         }
+    }
+
+    /**
+     * ログアウト処理を行う
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void logout() throws ServletException, IOException {
+
+        //セッションからログイン従業員のパラメータを削除
+        removeSessionScope(AttributeConst.LOGIN_EMP);
+
+        //セッションにログアウト時のフラッシュメッセージを追加
+        putSessionScope(AttributeConst.FLUSH, MessageConst.I_LOGOUT.getMessage());
+
+        //ログイン画面にリダイレクト
+        redirect(ForwardConst.ACT_AUTH, ForwardConst.CMD_SHOW_LOGIN);
+
     }
 
 }
